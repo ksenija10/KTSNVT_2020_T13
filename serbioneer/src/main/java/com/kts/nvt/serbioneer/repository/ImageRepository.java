f6c11328613bd9d0e690cc22dafb3e5efc765333
Igor Lukic <igi.l.1999@gmail.com>
2020-11-29 16:22:47 +0100

    #TUS-4-T-6 Implement image CRUD operations

--- serbioneer/src/main/java/com/kts/nvt/serbioneer/repository/ImageRepository.java
+++ serbioneer/src/main/java/com/kts/nvt/serbioneer/repository/ImageRepository.java
@@ -1,5 +1,7 @@
 package com.kts.nvt.serbioneer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,11 @@ import com.kts.nvt.serbioneer.model.Image;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+	List<Image> findAllByNewsId(Long newsId);
+
+	List<Image> findAllByCommentId(Long commentId);
+
+	List<Image> findAllByCulturalSiteId(Long culturalSiteId);
 
 }
--- serbioneer/src/main/java/com/kts/nvt/serbioneer/service/ImageService.java
+++ serbioneer/src/main/java/com/kts/nvt/serbioneer/service/ImageService.java
@@ -2,41 +2,116 @@ package com.kts.nvt.serbioneer.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kts.nvt.serbioneer.helper.exception.ForeignKeyException;
+import com.kts.nvt.serbioneer.helper.exception.NonexistentIdException;
+import com.kts.nvt.serbioneer.model.Comment;
+import com.kts.nvt.serbioneer.model.CulturalSite;
 import com.kts.nvt.serbioneer.model.Image;
+import com.kts.nvt.serbioneer.model.News;
+import com.kts.nvt.serbioneer.repository.ImageRepository;
 
 @Service
 public class ImageService implements ServiceInterface<Image> {
 
+	@Autowired
+	private ImageRepository imageRepository;
+
+	@Autowired
+	private CommentService commentService;
+
+	@Autowired
+	private NewsService newsService;
+
+	@Autowired
+	private CulturalSiteService culturalSiteService;
+
+	private final String type = "Image";
+
 	@Override
 	public List<Image> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return imageRepository.findAll();
+	}
+
+	List<Image> findAllByNewsId(Long newsId) {
+		return imageRepository.findAllByNewsId(newsId);
+	}
+
+	List<Image> findAllByCommentId(Long commentId) {
+		return findAllByCommentId(commentId);
+	}
+
+	List<Image> findAllByCulturalSiteId(Long culturalSiteId) {
+		return findAllByCulturalSiteId(culturalSiteId);
 	}
 
 	@Override
 	public Image findOneById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return imageRepository.findById(id).orElse(null);
 	}
 
 	@Override
 	public Image create(Image entity) throws Exception {
-		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Image create(Comment comment, Image entity) throws Exception {
+		Comment maybeComment = commentService.findOneById(comment.getId());
+		if (maybeComment == null) {
+			// throw new NonexistentIdException(commentService.getType());
+			throw new NonexistentIdException("Comment");
+		}
+		return imageRepository.save(entity);
+	}
+
+	public Image create(News news, Image entity) throws Exception {
+		News maybeNews = newsService.findOneById(news.getId());
+		if (maybeNews == null) {
+			// throw new NonexistentIdException(newsService.getType());
+			throw new NonexistentIdException("News");
+		}
+		return imageRepository.save(entity);
+	}
+
+	public Image create(CulturalSite culturalSite, Image entity) throws Exception {
+		CulturalSite maybeCulturalSite = culturalSiteService.findOneById(culturalSite.getId());
+		if (maybeCulturalSite == null) {
+			// throw new NonexistentIdException(newsService.getType());
+			throw new NonexistentIdException("Cultural Site");
+		}
+		return imageRepository.save(entity);
 	}
 
 	@Override
 	public void delete(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		
+		Image imageToDelete = imageRepository.findById(id).orElse(null);
+		if (imageToDelete == null) {
+			throw new NonexistentIdException(this.type);
+		}
+		if (imageToDelete.getComment() == null) {
+			throw new ForeignKeyException(this.type);
+		}
+		if (imageToDelete.getCulturalSite() == null) {
+			throw new ForeignKeyException(this.type);
+		}
+		if (imageToDelete.getNews() == null) {
+			throw new ForeignKeyException(this.type);
+		}
+		imageRepository.delete(imageToDelete);
+
 	}
 
 	@Override
 	public Image update(Image entity, Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Image imageToUpdate = imageRepository.findById(id).orElse(null);
+		if (imageToUpdate == null) {
+			throw new NonexistentIdException(this.type);
+		}
+		imageToUpdate.setContent(entity.getContent());
+		imageToUpdate.setName(entity.getName());
+		return imageRepository.save(imageToUpdate);
 	}
 
 }
