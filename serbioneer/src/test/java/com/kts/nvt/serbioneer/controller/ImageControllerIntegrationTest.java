6a3abdfd3c6abfff1040df9ec51f72f6bc5438dd
Igor Lukic <igi.l.1999@gmail.com>
2020-12-20 17:25:09 +0100

    Add ImageControllerIntegrationTest class

---
+++ serbioneer/src/test/java/com/kts/nvt/serbioneer/controller/ImageControllerIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.kts.nvt.serbioneer.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public class ImageControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+}
