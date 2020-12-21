343fc7abd600d0420b55a159ba0d2034ef37be07
Igor Lukic <igi.l.1999@gmail.com>
2020-12-21 22:07:56 +0100

    Add RatingServiceUnitTest

---
+++ serbioneer/src/test/java/com/kts/nvt/serbioneer/constants/RatingConstants.java
@@ -0,0 +1,47 @@
+package com.kts.nvt.serbioneer.constants;
+
+import com.kts.nvt.serbioneer.model.*;
+
+public class RatingConstants {
+
+    public static final Integer PAGEABLE_PAGE = 0;
+    public static final Integer PAGEABLE_SIZE = 2;
+    public static final Integer PAGEABLE_TOTAL_ELEMENTS = 1;
+
+    public static final Long CULTURAL_SITE_ID = 1L;
+    public static final Long CULTURAL_SITE_ID_RANDOM = 100L;
+    public static final Long USER_ID = 2L;
+    public static final Long RATING_ID = 1L;
+    public static final Long RATING_ID_RANDOM = 100L;
+
+
+    public static final CulturalSite CULTURAL_SITE = new CulturalSite("site1", "address1", "city1",
+            new CulturalCategoryType("categoryType1"),
+            new CulturalSiteCategory("category1")
+    );
+
+
+
+
+
+    public static final Long ADMIN_ID = 1L;
+    public static final String ADMIN_USERNAME = "admin@admin.com";
+    public static final String ADMIN_PASSWORD = "admin";
+
+    public static final String USER_USERNAME = "prvi@user.com";
+    public static final String USER_PASSWORD = "user";
+
+    public static final AuthenticatedUser AUTHENTICATED_USER = new AuthenticatedUser(2L,"","",USER_USERNAME,
+            "",true);
+
+    public static final Rating NEW_RATING = new Rating(4, CULTURAL_SITE, AUTHENTICATED_USER);
+
+    public static final Rating RATING = new Rating(5,
+            new CulturalSite("site1", "address1", "city1",
+                    new CulturalCategoryType("categoryType1"),
+                    new CulturalSiteCategory("category1")
+            ),
+            AUTHENTICATED_USER
+    );
+
+}
