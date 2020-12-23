fdd66e788ab69d941a8bbfb19b4d2253b548809e
fmilica <milicafilipovic@uns.ac.rs>
2020-12-23 19:15:12 +0100

    #TUS-4-T-10 AuthenticatedUser CRUD operations

--- serbioneer/src/test/java/com/kts/nvt/serbioneer/SuiteAll.java
+++ serbioneer/src/test/java/com/kts/nvt/serbioneer/SuiteAll.java
@@ -6,13 +6,16 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.test.context.TestPropertySource;
 
 import com.kts.nvt.serbioneer.controller.AdminControllerIntegrationTest;
+import com.kts.nvt.serbioneer.controller.AuthenticatedUserControllerIntegrationTest;
 import com.kts.nvt.serbioneer.repository.AdminRepositoryIntegationTest;
+import com.kts.nvt.serbioneer.repository.AuthenticatedUserRepositoryIntegrationTest;
 import com.kts.nvt.serbioneer.service.AdminServiceIntegrationTest;
+import com.kts.nvt.serbioneer.service.AuthenticatedUserServiceIntegrationTest;
 
 @RunWith(Suite.class)
-/*AdminServiceIntegrationTest.class, 
-		AdminServiceUnitTest.class*/
-@SuiteClasses({AdminControllerIntegrationTest.class, AdminRepositoryIntegationTest.class, AdminServiceIntegrationTest.class})
+@SuiteClasses({AdminControllerIntegrationTest.class, AdminRepositoryIntegationTest.class, AdminServiceIntegrationTest.class,
+	AuthenticatedUserControllerIntegrationTest.class, AuthenticatedUserRepositoryIntegrationTest.class,
+	AuthenticatedUserServiceIntegrationTest.class})
 @TestPropertySource("classpath:test.properties")
 public class SuiteAll {
 
