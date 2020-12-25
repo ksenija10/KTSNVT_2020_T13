bd5cc80591de8044797030540c2cb0e4f10fe0ca
Igor Lukic <igi.l.1999@gmail.com>
2020-12-25 20:19:21 +0100

    Add SecureUserDetailsService integration tests

---
+++ serbioneer/src/test/java/com/kts/nvt/serbioneer/constants/SecureUserDetailsConstants.java
@@ -0,0 +1,8 @@
+package com.kts.nvt.serbioneer.constants;
+
+public class SecureUserDetailsConstants {
+
+    public static final String USER_USERNAME_ACTIVATED = "prvi@user.com";
+    public static final String USER_USERNAME_NOT_ACTIVATED = "treci@user.com";
+
+}
