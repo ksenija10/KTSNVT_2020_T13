74d6f8d15c17667993acca4886ae976eb060a21a
Igor Lukic <igi.l.1999@gmail.com>
2020-12-25 19:54:36 +0100

    Add RefreshController integration tests

---
+++ serbioneer/src/test/java/com/kts/nvt/serbioneer/controller/RefreshControllerIntegrationTest.java
@@ -0,0 +1,78 @@
+package com.kts.nvt.serbioneer.controller;
+
+import com.kts.nvt.serbioneer.dto.CulturalSiteDTO;
+import com.kts.nvt.serbioneer.dto.UserLoginDTO;
+import com.kts.nvt.serbioneer.helper.HelperPage;
+import com.kts.nvt.serbioneer.jwt.TokenUtils;
+import com.kts.nvt.serbioneer.service.SecureUserDetailsService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.kts.nvt.serbioneer.constants.CulturalSiteConstants.FILTER_NO_NUM;
+import static com.kts.nvt.serbioneer.constants.RefreshConstants.ADMIN_PASSWORD;
+import static com.kts.nvt.serbioneer.constants.RefreshConstants.ADMIN_USERNAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public class RefreshControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @Autowired
+    private SecureUserDetailsService userDetailsService;
+
+    private String accessToken;
+
+    public void login(String username, String password) {
+        //poziv REST servisa za logovanje
+        ResponseEntity<UserLoginDTO> responseEntity = restTemplate.postForEntity("/login",
+                new UserLoginDTO(username, password), UserLoginDTO.class);
+        //postavljanje authorization tokena
+        this.accessToken = responseEntity.getHeaders().get("Authorization").get(0);
+    }
+
+    @Test
+    public void testRefreshAuthenticationToken(){
+        login(ADMIN_USERNAME, ADMIN_PASSWORD);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", this.accessToken);
+        HttpEntity<Object> httpEntity = new HttpEntity<Object>(headers);
+
+        ResponseEntity<Void> responseEntity = restTemplate.exchange(
+                "/api/refresh", HttpMethod.POST, httpEntity,
+                new ParameterizedTypeReference<Void>() {});
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+
+    @Test
+    public void testRefreshAuthenticationTokenUnauthorized(){
+        login(ADMIN_USERNAME, ADMIN_PASSWORD);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "this.accessToken");
+        HttpEntity<Object> httpEntity = new HttpEntity<Object>(headers);
+
+        ResponseEntity<Void> responseEntity = restTemplate.exchange(
+                "/api/refresh", HttpMethod.POST, httpEntity,
+                new ParameterizedTypeReference<Void>() {});
+
+        assertEquals(HttpStatus.UNAUTHORIZED, responseEntity.getStatusCode());
+    }
+}
