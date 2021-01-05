a8ddec36d014ed6604b91332af64d92da504ba2a
ksenija10 <ksenija.prcic1998@gmail.com>
2021-01-05 16:48:12 +0100

    #US-1-T-2
    Created registration form

--- serbioneer-front/src/app/components/login-register/register/register.component.ts
+++ serbioneer-front/src/app/components/login-register/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormsModule, ValidatorFn } from '@angular/forms';
+import { confirmPasswordValidator } from 'src/app/directives/confirm-password.directive';
+import { AuthenticatedUser } from 'src/app/model/authenticated-user.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+    ) {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      surname: new FormControl('', [Validators.required])
+    },
+    {validators: confirmPasswordValidator()}
+    );
+   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+    if(this.registerForm.invalid) {
+      return;
+    }
+
+    const authUser: AuthenticatedUser = 
+      new AuthenticatedUser(
+        this.registerForm.value.email, 
+        this.registerForm.value.password, 
+        this.registerForm.value.name, 
+        this.registerForm.value.surname, 
+        )
+    
+    this.authenticationService.register(authUser)
+      .subscribe(
+        response => {
+          console.log(response);
+          // ovde mu reci hej na mejl ti je stigao mejl, prati ga da se registrujes
+          this.registerForm.reset();
+          this.router.navigate(['login-register/login']);
+        },
+        error => {
+          console.log(error);
+        })
   }
 
 }
---
+++ serbioneer-front/src/app/model/authenticated-user.model.ts
@@ -0,0 +1,9 @@
+export class AuthenticatedUser{
+
+    constructor(
+        public email: string,
+        public password: string,
+        public name: string, 
+        public surname: string) {}
+
+}
