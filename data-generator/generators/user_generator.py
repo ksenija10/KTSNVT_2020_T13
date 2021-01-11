ebf6c784221cf5577a7c036e2765b66ccc6a26ad
Igor Lukic <igi.l.1999@gmail.com>
2021-01-11 17:32:39 +0100

    Add user generator

---
+++ data-generator/generators/user_generator.py
@@ -0,0 +1,40 @@
+from models.user import User
+import bcrypt
+import random
+import datetime
+
+
+class UserGenerator:
+    def __init__(self, data_path, output_path):
+        self.data_path = data_path
+        self.output_path = output_path
+
+    def generate(self):
+        input_file = open(self.data_path, 'r', encoding="utf-8")
+        people = []
+        line = input_file.readline()
+        while line != "":
+            people.append((line.split(" ")[0], line.split(" ")[1].rstrip("\n")))
+            line = input_file.readline()
+        input_file.close()
+
+        output_text = ""
+        for i in range(50):
+            admin = User("users", "Admin", people[i][0], people[i][1], str(random.randint(1000, 9999)),
+                         "true", generate_date())
+            output_text += admin.generate_insert()+"\n\n"
+        for i in range(50, len(people)):
+            user = User("users", "AuthenticatedUser", people[i][0], people[i][1], str(random.randint(1000, 9999)),
+                        "true", "true")
+            output_text += user.generate_insert()+"\n\n"
+
+        output_file = open(self.output_path, "w", encoding="utf-8")
+        output_file.write(output_text)
+        output_file.close()
+
+
+def generate_date():
+    year = random.randint(1900, 2000)
+    month = random.randint(1, 12)
+    day = random.randint(1, 28)
+    return datetime.date(year, month, day)
---
+++ data-generator/models/user.py
@@ -0,0 +1,66 @@
+import bcrypt
+
+
+class User:
+    def __init__(self, table_name, user_type, name, surname, password, is_active, birthday=None, activated=None):
+        self.table_name = table_name
+        self.user_type = user_type
+        self.name = name
+        self.surname = surname
+        self.email = self.generate_email()
+        self.password = password
+        self.is_active = is_active
+        self.activated = activated
+        self.birthday = birthday
+
+    def generate_insert(self):
+        query = "--email = " + self.email + " password = " + self.password + "\n"
+        if self.birthday is None:
+            query += "insert into " + self.table_name + " (type, name, surname, email, password, is_active, activated) "
+            query += "values ('" + self.user_type + "', '" + self.name + "', '" + self.surname + "', '" + self.email + "', '" + self.encode_password() \
+                     + "', '" + self.is_active + "', '" + self.activated + "');"
+        else:
+            query += "insert into " + self.table_name + " (type, name, surname, email, password, is_active, birthday) "
+            query += "values ('" + self.user_type + "', '" + self.name + "', '" + self.surname + "', '" + self.email + "', '" + self.encode_password() \
+                     + "', '" + self.is_active + "', '" + str(self.birthday) + "');"
+        return query
+
+    def generate_email(self):
+        ascii_name = ""
+        for char in self.name:
+            ascii_name += from_utf8_to_ascii(char)
+        ascii_surname = ""
+        for char in self.surname:
+            ascii_surname += from_utf8_to_ascii(char)
+
+        email = self.name + "." + self.surname + "@email.com"
+        return email.lower()
+
+    def encode_password(self):
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(self.password.encode("utf-8"), salt).decode("utf-8")
+
+
+def from_utf8_to_ascii(char):
+    if char == "Ć":
+        return "C"
+    elif char == "ć":
+        return "c"
+    elif char == "Č":
+        return "C"
+    elif char == "č":
+        return "c"
+    elif char == "Š":
+        return "S"
+    elif char == "š":
+        return "s"
+    elif char == "Ž":
+        return "Z"
+    elif char == "ž":
+        return "z"
+    elif char == "Đ":
+        return "Dj"
+    elif char == "đ":
+        return "dj"
+    else:
+        return char
