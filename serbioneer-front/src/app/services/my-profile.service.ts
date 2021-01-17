ed5954909d54e943ea45ffa847f1e605163a48c9
ksenija10 <ksenija.prcic1998@gmail.com>
2021-01-17 22:22:55 +0100

    #US-27 my profile
    My profile service angular unit testing implemented

--- serbioneer-front/src/app/services/my-profile.service.ts
+++ serbioneer-front/src/app/services/my-profile.service.ts
@@ -4,7 +4,6 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { AuthenticatedUser } from "../model/authenticated-user.model";
 import { PasswordDTO } from "../model/password-dto.model";
-import { UserLogin } from "../model/user-login.model";
 import { UserUpdateDTO } from "../model/user-update-dto.mpdel";
 import { AuthenticationService } from "./authentication.service";
 
@@ -15,19 +14,15 @@ export class MyProfileService{
 
     private headers = new HttpHeaders({'Content-Type': 'application/json'});
 
-    private userApi: string;
+    private userApi: string = '';
 
     constructor(
         private http: HttpClient,
         private authenticationService: AuthenticationService
-    ) {
-        this.userApi = 'authenticated-user/';
-        if(this.authenticationService.getLoggedInUserAuthority() === 'ROLE_ADMIN') {
-            this.userApi = 'admin/';
-        }
-    }
+    ) {}
 
     updatePersonalInformation(userUpdateDto: UserUpdateDTO): Observable<any> {
+        this.checkRole();
         return this.http.put<UserUpdateDTO>(
             environment.apiEndpoint + this.userApi + 'updatePersonalInformation',
             userUpdateDto,
@@ -36,6 +31,7 @@ export class MyProfileService{
     }
 
     getCurrentAuthenticatedUser(): Observable<any> {
+        this.checkRole();
         return this.http.get<AuthenticatedUser>(
             environment.apiEndpoint + this.userApi + 'view-profile',
             { headers: this.headers}
@@ -43,10 +39,19 @@ export class MyProfileService{
     }
 
     updatePassword(passwordDto: PasswordDTO) {
+        this.checkRole();
         return this.http.put(
             environment.apiEndpoint + this.userApi + 'updatePassword',
             passwordDto,
             { headers: this.headers, observe: 'response'}
         )
+    }
+
+    private checkRole() {
+        if(this.authenticationService.getLoggedInUserAuthority() === 'ROLE_ADMIN') {
+            this.userApi = 'admin/';
+        } else {
+            this.userApi = 'authenticated-user/';
+        }
     }
 }
