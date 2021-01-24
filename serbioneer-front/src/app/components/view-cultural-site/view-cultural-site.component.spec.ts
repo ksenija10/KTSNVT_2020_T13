c18d47a305f588a8ef2807f9c94f0c935dee18ed
fmilica <milicafilipovic@uns.ac.rs>
2021-01-24 20:35:18 +0100

    - detailed view of cultural site angular testing

--- serbioneer-front/src/app/components/view-cultural-site/view-cultural-site.component.spec.ts
+++ serbioneer-front/src/app/components/view-cultural-site/view-cultural-site.component.spec.ts
@@ -17,6 +17,7 @@ import { CulturalSiteService } from "src/app/services/cultural-site.service";
 import { ImageService } from "src/app/services/image.service";
 import { RatingCreateDTO, RatingService } from "src/app/services/rating.service";
 import { ViewCulturalSiteComponent } from "./view-cultural-site.component";
+import { MatInputHarness } from "@angular/material/input/testing";
 
 fdescribe('ViewCulturalSiteComponent', () => {
     let component: ViewCulturalSiteComponent;
@@ -192,6 +193,7 @@ fdescribe('ViewCulturalSiteComponent', () => {
         loader = TestbedHarnessEnvironment.loader(fixture);
         // izaziva inicijalni data binding
         fixture.detectChanges();
+        newsDialog = TestBed.inject(MatDialog);
     })
 
     it('should load instance', () => {
@@ -336,4 +338,214 @@ fdescribe('ViewCulturalSiteComponent', () => {
         expect(component.buttonValue).toEqual('Subscribe');
     })
 
+    it('should not get text error message', ( async () => {
+        component.myForm.value.text = "Sabacka Biblioteka was cool."
+        component.myForm.controls['text'].setErrors(null);
+      
+        let message = component.getTextErrorMessage();
+        
+        expect(component.myForm.invalid).toBeFalsy();
+        expect(component.myForm.controls['text'].hasError('required')).toBeFalsy()
+        expect(message).toEqual('');
+    }))
+
+    it('should get text error message', ( async () => {
+        component.myForm.controls['text'].markAsTouched();
+        
+        let message = component.getTextErrorMessage();
+        
+        expect(component.myForm.invalid).toBeTruthy();
+        expect(component.myForm.controls['text'].hasError('required')).toBeTruthy()
+        expect(message).toEqual('Required field');
+    }))
+
+    it('should delete cultural site', ( () => {
+        component.ngOnInit();
+
+        component.deleteCulturalSite();
+
+        expect(culturalSiteService.deleteCulturalSite).toHaveBeenCalledWith(1);
+        expect(toastr.success).toHaveBeenCalledWith('Successfully deleted cultural site!');
+        expect(router.navigate).toHaveBeenCalledWith(['homepage']);
+    }))
+
+    it('should edit cultural site', ( async () => {
+        component.editCulturalSite();
+
+        expect(router.navigate).toHaveBeenCalledWith(['edit-cultural-site/1']);
+    }))
+
+    it('should submit images', ( async () => {
+        component.ngOnInit();
+        component.newImageFiles = [{name: 'image1', content: '1'}];
+        component.culturalSite.images = [];
+
+        component.submitImages();
+
+        expect(imageService.createForCulturalSite).toHaveBeenCalledWith(1, {name: 'image1', content: '1'});
+        expect(component.newImages.length).toEqual(0);
+        expect(component.newImageFiles.length).toEqual(0);
+        expect(component.addNewImages).toEqual(false);
+        expect(component.culturalSite.images.length).toEqual(1);
+
+        expect(toastr.success).toHaveBeenCalledOnceWith('Successfully added image for cultural site!');
+    }))
+
+    it('should emit event for loading added images for cultural site', ( async () => {
+        let event = {
+            target: {
+                files: [
+                    new Blob([''], { type: 'text/html' }),
+                    new Blob([''], { type: 'text/html' })
+                ]
+            }
+        }
+
+        component.newImageFiles = [];
+        component.ngOnInit();
+
+        component.onNewImageChange(event);
+
+        expect(component.newImageFiles.length).toEqual(2);
+    }))
+
+    it('should add comments', ( () => {
+        component.ngOnInit();
+        component.addNewComment = false;
+
+        //TODO
+        /*const textInput = await loader.getHarness(
+            MatInputHarness.with({ selector: '#new-comment-input' })
+        );
+        await textInput.setValue('Previous comment');*/
+
+        component.addComments();
+
+        expect(component.addNewComment).toEqual(true);
+        expect(component.images.length).toEqual(0);
+        //expect(await textInput.getValue()).toEqual('');
+    }))
+
+    it('should add images', ( async () => {
+        component.ngOnInit();
+        component.addNewImages = false;
+
+        component.addImages();
+
+        expect(component.addNewImages).toEqual(true);
+        expect(component.newImages.length).toEqual(0);
+        expect(component.newImageFiles.length).toEqual(0);
+    }))
+
+    it('should open dialog', ( async () => {
+        component.ngOnInit();
+        
+        component.openDialog();
+
+        expect(newsDialog.open).toHaveBeenCalled();
+        expect(culturalSiteService.getAllCulturalSiteNews).toHaveBeenCalled();
+    }))
+
+    it('should create new comment', ( async () => {
+        component.myForm.setValue({
+            text: "Sabacka Biblioteka was cool.",
+            file: "",
+            fileSource: {}
+          });
+        component.culturalSite = {
+            id: 1
+        }
+        component.files = [{}]
+        component.submit();
+
+        expect(culturalSiteService.createComment).toHaveBeenCalledWith(1, "Sabacka Biblioteka was cool.");
+        expect(imageService.createForComment).toHaveBeenCalled();
+        expect(component.images.length).toEqual(0);
+        expect(component.files.length).toEqual(0);
+        expect(component.addNewComment).toEqual(false);
+        expect(culturalSiteService.getAllCulturalSiteComments).toHaveBeenCalled();
+        expect(toastr.success).toHaveBeenCalledOnceWith('Successfully reviewed cultural site!\n' +
+                                                        'Your review will be visible after approval.');
+        expect(component.addNewComment).toEqual(false);
+        //TODO reset forme
+    }))
+
+    it('should emit event for loading added images for comment', ( async () => {
+        let event = {
+            target: {
+                files: [
+                    new Blob([''], { type: 'text/html' }),
+                    new Blob([''], { type: 'text/html' })
+                ]
+            }
+        }
+
+        component.files = [];
+        component.ngOnInit();
+
+        component.onFileChange(event);
+
+        expect(component.files.length).toEqual(2);
+    }))
+
+    it('should create rating', ( async () => {
+        component.ngOnInit();
+        component.initialStarRating = 0;
+        component.starRating.value = 5;
+        component.culturalSite.id = 1;
+
+        component.onRatingChange();
+
+        expect(ratingService.createRating).toHaveBeenCalledOnceWith(1, 5);
+        expect(component.initialStarRating).toEqual(5);
+        expect(component.starRating.id).toEqual(1);
+        expect(component.starRating.value).toEqual(5);
+        expect(component.starRating.culturalSiteId).toEqual(1);
+        expect(component.starRating.authenticatedUserId).toEqual(1);
+
+        expect(culturalSiteService.getCulturalSite).toHaveBeenCalledWith(1);
+        expect(component.culturalSite.id).toEqual(1);
+        expect(component.culturalSite.name).toEqual('Kulturno dobro');
+        expect(component.culturalSite.categoryId).toEqual(1);
+        expect(component.culturalSite.category).toEqual('Kategorija');
+        expect(component.culturalSite.categoryTypeId).toEqual(1);
+        expect(component.culturalSite.categoryType).toEqual('Tip');
+        expect(component.culturalSite.lat).toEqual(42);
+        expect(component.culturalSite.lng).toEqual(20);
+        expect(component.culturalSite.address).toEqual('Adresa 5');
+        expect(component.culturalSite.city).toEqual('Grad');
+        expect(component.culturalSite.description).toEqual('Opis');
+        expect(component.culturalSite.rating).toEqual(5);
+    }))
+
+    it('should update rating', ( async () => {
+        component.ngOnInit();
+        component.initialStarRating = 3;
+        component.starRating = {
+            id: 1,
+            value: 5,
+            culturalSiteId: 1,
+            authenticatedUserId: 1
+        };
+        component.culturalSite.id = 1;
+
+        component.onRatingChange();
+
+        expect(ratingService.updateRating).toHaveBeenCalledOnceWith(1, 5, 1);
+        expect(component.initialStarRating).toEqual(5);
+
+        expect(culturalSiteService.getCulturalSite).toHaveBeenCalledWith(1);
+        expect(component.culturalSite.id).toEqual(1);
+        expect(component.culturalSite.name).toEqual('Kulturno dobro');
+        expect(component.culturalSite.categoryId).toEqual(1);
+        expect(component.culturalSite.category).toEqual('Kategorija');
+        expect(component.culturalSite.categoryTypeId).toEqual(1);
+        expect(component.culturalSite.categoryType).toEqual('Tip');
+        expect(component.culturalSite.lat).toEqual(42);
+        expect(component.culturalSite.lng).toEqual(20);
+        expect(component.culturalSite.address).toEqual('Adresa 5');
+        expect(component.culturalSite.city).toEqual('Grad');
+        expect(component.culturalSite.description).toEqual('Opis');
+        expect(component.culturalSite.rating).toEqual(5);
+    }))
 })
